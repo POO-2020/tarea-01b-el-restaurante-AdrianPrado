c2633187bb408813401059e85518621def5d6258
Macbook Air <macbookair@MacBook-Air-de-Macbook.local>
2020-02-26 16:43:47 -0600

    Prueba elemento pedido

---
+++ elemento-pedido.js
@@ -0,0 +1,17 @@
+import Producto from "./producto.js"
+import Precio from "./precio.js"
+export default class ElementoPedido{
+    /**
+     * 
+     * @param {Producto} producto 
+     * @param {number} cantidad 
+     */
+    constructor(producto, cantidad){
+        this.producto = producto
+        this.cantidad = cantidad
+    }
+    getDescripcion(){
+        let precioFinal = this.cantidad * this.producto.precios.valor
+        return `${this.cantidad} x ${this.producto.nombres} $${new Intl.NumberFormat("en-US").format(precioFinal)}`
+    }
+}
